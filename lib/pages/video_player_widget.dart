611fce768ba237909eb728e254cbb8ad433c78fc
hippieperm <ghl0617@gmail.com>
2024-08-01 17:07:27 +0900

    사진앱 접근권한/가져온이미지 화면에 표시 CocoaPods is installed in the project

---
+++ lib/pages/video_player_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:io';
+
+class VideoPlayerWidget extends StatefulWidget {
+  final File? file;
+
+  const VideoPlayerWidget({Key? key, this.file}) : super(key: key);
+
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(widget.file!)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+        : const Center(child: CircularProgressIndicator());
+  }
+}
