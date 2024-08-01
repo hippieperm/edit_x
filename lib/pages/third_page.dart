578e698a509231f6eb0845c823d970993796531b
hippieperm <ghl0617@gmail.com>
2024-08-01 12:17:12 +0900

    사진앱 접근권한/가져온이미지 화면에 표시

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -1,27 +1,40 @@
-import 'package:edit_x/common_widgets/common_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-import '../styles/app_colors.dart';
-
-class ThirdPage extends StatelessWidget {
+class ThirdPage extends StatefulWidget {
   const ThirdPage({super.key});
+
+  @override
+  _ThirdPageState createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _image = File(image.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CommonAppBar(
+      appBar: AppBar(
         title: const Text('Third Page'),
-        backgroundColor: AppColors.battery,
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'You are on the Third Page',
-              ),
+              const Text('You are on the Third Page'),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
@@ -35,7 +48,14 @@ class ThirdPage extends StatelessWidget {
                 },
                 child: const Text('Go to Home Page'),
               ),
-              SizedBox(height: 1000), // 추가된 콘텐츠로 인해 스크롤 가능하도록
+              SizedBox(height: 20),
+              _image == null
+                  ? const Text('No image selected.')
+                  : Image.file(_image!),
+              ElevatedButton(
+                onPressed: _pickImage,
+                child: const Text('Pick Image from Gallery'),
+              ),
             ],
           ),
         ),
