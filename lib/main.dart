2875b3a07fa54ed49559cea0802389672fa54c9b
hippieperm <ghl0617@gmail.com>
2024-10-07 00:26:23 +0900

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      //test
+      //test,
       initialRoute: '/',
       getPages: [
         GetPage(name: '/', page: () => const HomePage()),
